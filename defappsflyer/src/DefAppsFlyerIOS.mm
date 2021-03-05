3a8c933e16085f7838cd4dfbe3fe2dcd3134fa1f
Андрей Набока <a.naboka@mpgames.rocks>
2021-03-05 16:42:35 +0300

    Change api

--- defappsflyer/src/DefAppsFlyerIOS.mm
+++ defappsflyer/src/DefAppsFlyerIOS.mm
@@ -6,23 +6,23 @@
 
 void DefAppsFlyer_setAppsFlyerKey(const char*appsFlyerKey)
 {
-  [AppsFlyerTracker sharedTracker].appsFlyerDevKey = [NSString stringWithUTF8String:appsFlyerKey];
+  [AppsFlyerLib shared].appsFlyerDevKey = [NSString stringWithUTF8String:appsFlyerKey];
   //[AppsFlyerTracker sharedTracker].delegate = self;
 }
 
 void DefAppsFlyer_setAppID(const char*appleAppID)
 {
-  [AppsFlyerTracker sharedTracker].appleAppID = [NSString stringWithUTF8String:appleAppID];
+  [AppsFlyerLib shared].appleAppID = [NSString stringWithUTF8String:appleAppID];
 }
 
 void DefAppsFlyer_setIsDebug(bool is_debug)
 {
-  [AppsFlyerTracker sharedTracker].isDebug = is_debug;
+  [AppsFlyerLib shared].isDebug = is_debug;
 }
 
 void DefAppsFlyer_trackAppLaunch()
 {
-  [[AppsFlyerTracker sharedTracker] trackAppLaunch];
+  [[AppsFlyerLib shared] start];
 }
 
 
@@ -41,7 +41,7 @@ void DefAppsFlyer_trackEvent(const char*eventName, dmArray<TrackData>* trackData
       value = [NSString stringWithUTF8String: data.value];
       newDict[key] = value;
     }
-    [[AppsFlyerTracker sharedTracker] trackEvent: event withValues: newDict];
+    [[AppsFlyerLib shared] logEvent: event withValues: newDict];
   }
 }
 
